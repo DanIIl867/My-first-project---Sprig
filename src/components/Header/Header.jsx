656b624e62819714f156bbbaf418a2110ab00363
Daniil <negativdaniil4@gmail.com>
2024-07-03 22:30:54 +0300

    my-first-project-sprig

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,23 @@
+import { Conteiner, Logo, Nav, Info } from './Header.styled'
+
+
+
+export const Header = () => {
+    return (
+        <>
+            <section>
+                <Conteiner>
+                    <Logo>
+                        <img src="" alt="" />
+                    </Logo>
+                    <Nav>
+
+                    </Nav>
+                    <Info>
+
+                    </Info>
+                </Conteiner>
+            </section>
+        </>
+    )
+}
---
+++ src/components/Header/Header.styled.jsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const Conteiner = styled.div`
+    height: 83px;
+    display: flex;
+`
+
+export const Logo = styled.div`
+    
+`
+
+export const Nav = styled.div`
+    
+`
+
+export const Info = styled.div`
+    
+`
