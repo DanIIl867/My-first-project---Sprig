c96367a4d173bf5c0cf188d68a30271920fddda2
Daniil <negativdaniil4@gmail.com>
2024-07-04 21:50:09 +0300

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,5 @@
-import { Conteiner, Logo, Nav, Info } from './Header.styled'
+import { Conteiner, Logo, Nav, Info } from './Header.styled';
+import LogoType from '../../img/frame.png';
 
 
 
@@ -8,16 +9,20 @@ export const Header = () => {
             <section>
                 <Conteiner>
                     <Logo>
-                        <img src="" alt="" />
+                        <img src={LogoType} alt="" />
                     </Logo>
                     <Nav>
 
                     </Nav>
                     <Info>
-
+                        <a href="/" className='button1'>Sign In</a>
+                        <a href="/" className='button2'>Talk to Sales</a>
+                        <a href="/" className='button3'>Start for Free</a>
                     </Info>
                 </Conteiner>
             </section>
         </>
     )
 }
+
+export default Header;
