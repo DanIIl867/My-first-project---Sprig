b097d19c2b6e8a21fd12249278314cd0578fe2a1
Daniil <negativdaniil4@gmail.com>
2024-07-04 22:32:22 +0300

    my-first-project-sprig

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import { Conteiner, Logo, Nav, Info } from './Header.styled';
+import { Conteiner, Logo, Nav, Info, } from './Header.styled';
 import LogoType from '../../img/frame.png';
 
 
@@ -12,12 +12,23 @@ export const Header = () => {
                         <img src={LogoType} alt="" />
                     </Logo>
                     <Nav>
-
+                        <div className='why-sprig'>
+                            <a href="/">Why Sprig?</a>
+                        </div>
+                        <div className='dropMenu'>
+                            <ul>
+                                <li><a href="/">Product</a></li>
+                                <li><a href="/">Customers</a></li>
+                                <li><a href="/">Resources</a></li>
+                            </ul>
+                        </div>
                     </Nav>
                     <Info>
-                        <a href="/" className='button1'>Sign In</a>
-                        <a href="/" className='button2'>Talk to Sales</a>
-                        <a href="/" className='button3'>Start for Free</a>
+                        <ul>
+                            <li><a href="/" className='btn button-1'>Sign In</a></li>
+                            <li><a href="/" className='btn button-2'>Talk to Sales</a></li>
+                            <li><a href="/" className='btn button-3'>Start for Free</a></li>
+                        </ul>
                     </Info>
                 </Conteiner>
             </section>
--- src/components/Header/Header.styled.jsx
+++ src/components/Header/Header.styled.jsx
@@ -14,5 +14,16 @@ export const Nav = styled.div`
 `
 
 export const Info = styled.div`
-    
+    /* display: flex;
+    gap: 20px;
+
+    .btn{
+        text-decoration: none;
+        color: black;
+
+        .btn:hover{
+            border: 1px solid rgba(11, 35, 48, 1);
+            background-color: rgba(11, 35, 48, 1);
+        }
+    } */
 `
