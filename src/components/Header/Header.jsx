a1ceeaeb91bd8baba767194637c4f24785cb4af3
Daniil <negativdaniil4@gmail.com>
2024-07-09 20:52:53 +0300

    my-first-project-sprig

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,37 +1,32 @@
-import { Conteiner, Logo, Nav, Info, } from './Header.styled';
+import s from"./Header.module.css";
 import LogoType from '../../img/frame.png';
-
+import arrow from '../../img/Vector.svg';
 
 
 export const Header = () => {
     return (
         <>
-            <section>
-                <Conteiner>
-                    <Logo>
-                        <img src={LogoType} alt="" />
-                    </Logo>
-                    <Nav>
-                        <div className='why-sprig'>
-                            <a href="/">Why Sprig?</a>
-                        </div>
-                        <div className='dropMenu'>
-                            <ul>
-                                <li><a href="/">Product</a></li>
-                                <li><a href="/">Customers</a></li>
-                                <li><a href="/">Resources</a></li>
-                            </ul>
-                        </div>
-                    </Nav>
-                    <Info>
-                        <ul>
-                            <li><a href="/" className='btn button-1'>Sign In</a></li>
-                            <li><a href="/" className='btn button-2'>Talk to Sales</a></li>
-                            <li><a href="/" className='btn button-3'>Start for Free</a></li>
-                        </ul>
-                    </Info>
-                </Conteiner>
-            </section>
+            <div className={s.container}>
+                <div className={s.logo}>
+                    <a href="/"><img src={LogoType} alt="" /></a>
+                </div>
+                <div className={s.nav}>
+                    <ul>
+                        <li><a href="/">Why Sprig?</a></li>
+                        <li><a href="/">Product </a><img src={arrow} alt="" /></li>
+                        <li><a href="/">Customers </a><img src={arrow} alt="" /></li>
+                        <li><a href="/">Resources </a><img src={arrow} alt="" /></li>
+                        <li><a href="/">Plans</a></li>
+                    </ul>
+                </div>
+                <div className={s.info}>
+                    <ul>
+                        <li><a href="/" className={s.button1}>Sign In</a></li>
+                        <li><a href="/" className={s.button2}>Talk to Sales</a></li>
+                        <li><a href="/" className={s.button3}>Start for Free</a></li>
+                    </ul>
+                </div>
+            </div>
         </>
     )
 }
