f2962126c3f9368587dfbca13634e672764f98ac
Daniil <negativdaniil4@gmail.com>
2024-08-22 19:44:02 +0300

    Update FooterTop.jsx

--- src/components/FooterTop/FooterTop.jsx
+++ src/components/FooterTop/FooterTop.jsx
@@ -3,7 +3,18 @@ import s from "./FooterTop.module.css"
 export const FooterTop = () => {
     return(
         <div className={s.section}>
-            
+            <div className={s.container}>
+                <div className={s.textBlock}>
+                    <div className={s.heading}>
+                        <h2>Launch a Sprig within minutes.
+                            See insights within hours.
+                        </h2>
+                    </div>
+                    <div className={s.paragraph}>
+                        <p></p>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
